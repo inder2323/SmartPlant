25f3b3a894c19c55718a7bb4f865f43e24cdf326
inder2323 <52960680+inder2323@users.noreply.github.com>
2019-08-24 10:20:34 +1000

    Add files via upload

---
+++ myfile.ino
@@ -0,0 +1,207 @@
+// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+#include "RTClib.h"
+
+RTC_DS1307 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+//Libraries
+#include "SD.h"
+#include <Wire.h>
+
+
+#define LOG_INTERVAL  1000 // mills between entries. 
+// how many milliseconds before writing the logged data permanently to disk
+// set it to the LOG_INTERVAL to write each time (safest)
+// set it to 10*LOG_INTERVAL to write all data every 10 datareads, you could lose up to
+// the last 10 reads if power is lost but it uses less power and is much faster!
+#define SYNC_INTERVAL 1000 // mills between calls to flush() - to write data to the card
+uint32_t syncTime = 0; // time of last sync()
+
+
+/*
+  determines whether to send the stuff thats being written to the card also out to the Serial monitor.
+  This makes the logger a little more sluggish and you may want the serial monitor for other stuff.
+  On the other hand, its hella useful. We'll set this to 1 to keep it on. Setting it to 0 will turn it off.
+
+*/
+#define ECHO_TO_SERIAL   1 // echo data to serial port. 
+
+
+// for the data logging shield, we use digital pin 10 for the SD cs line
+const int chipSelect = 10;
+
+// the logging file
+File logfile;
+ int LED = 7; 
+
+void setup()
+{
+  Serial.begin(57600);
+  pinMode(LED,OUTPUT);
+
+  // initialize the SD card
+  initSDcard();
+  createFile();
+  initRTC();
+  logfile.println("millis,stamp,datetime,Moisture level");
+#if ECHO_TO_SERIAL
+  Serial.println("millis,stamp,datetime,Moisture level");
+#endif //ECHO_TO_SERIAL
+  while (!Serial); // for Leonardo/Micro/Zero
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  rtc.adjust(DateTime(2019,8,22,3,2,2));
+}
+
+
+void loop()
+{
+  DateTime now = rtc.now();
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+     
+    Serial.println();
+    delay(1000);
+  // delay for the amount of time we want between readings
+  delay((LOG_INTERVAL - 1) - (millis() % LOG_INTERVAL));
+
+  // log milliseconds since starting
+  uint32_t m = millis();
+  logfile.print(m);           // milliseconds since start
+  logfile.print(", ");
+#if ECHO_TO_SERIAL
+  Serial.print(m);         // milliseconds since start
+  Serial.print(", ");
+#endif
+
+  // fetch the time
+  now = rtc.now();
+  // log time
+  logfile.print(now.unixtime()); // seconds since 2000
+  logfile.print(", ");
+  logfile.print(now.year(), DEC);
+  logfile.print("/");
+  logfile.print(now.month(), DEC);
+  logfile.print("/");
+  logfile.print(now.day(), DEC);
+  logfile.print(" ");
+  logfile.print(now.hour(), DEC);
+  logfile.print(":");
+  logfile.print(now.minute(), DEC);
+  logfile.print(":");
+  logfile.print(now.second(), DEC);
+#if ECHO_TO_SERIAL
+  Serial.print(now.unixtime()); // seconds since 2000
+  Serial.print(", ");
+  Serial.print(now.year(), DEC);
+  Serial.print("/");
+  Serial.print(now.month(), DEC);
+  Serial.print("/");
+  Serial.print(now.day(), DEC);
+  Serial.print(" ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(":");
+  Serial.print(now.minute(), DEC);
+  Serial.print(":");
+  Serial.print(now.second(), DEC);
+#endif //ECHO_TO_SERIAL
+
+  int val;
+  val = analogRead(0); //connect sensor to Analog 0
+  Serial.println(val); //print the value to serial port
+  if( val <= 300 )
+  { digitalWrite(LED,HIGH);
+  }
+  else
+  { digitalWrite(LED,LOW);
+  }
+
+  logfile.print(", ");
+  logfile.println(val);
+
+#if ECHO_TO_SERIAL
+  Serial.print(", ");
+  Serial.println(val);
+
+#endif //ECHO_TO_SERIAL
+
+  if ((millis() - syncTime) < SYNC_INTERVAL) return;
+  syncTime = millis();
+
+  logfile.flush();
+}
+
+void error(char const *str)
+{
+  Serial.print("error: ");
+  Serial.println(str);
+
+  while (1);
+}
+
+void initSDcard()
+{
+  Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+ else Serial.println("card initialized.");
+
+}
+
+void createFile()
+{
+  //file name must be in 8.3 format (name length at most 8 characters, follwed by a '.' and then a three character extension.
+  char filename[] = "MLOG00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[4] = i / 10 + '0';
+    filename[5] = i % 10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE);
+      break;  // leave the loop!
+    }
+  }
+
+  if (! logfile) {
+    error("couldnt create file");
+  }
+
+  Serial.print("Logging to: ");
+  Serial.println(filename);
+}
+
+void initRTC()
+{
+  Wire.begin();
+  if (!rtc.begin()) {
+    logfile.println("RTC failed");
+#if ECHO_TO_SERIAL
+    Serial.println("RTC failed");
+#endif  //ECHO_TO_SERIAL
+
+  }
+}
